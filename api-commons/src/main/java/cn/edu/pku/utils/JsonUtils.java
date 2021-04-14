f3a9a1d1aaed30d4a9be92c61cfef7e816f66837
tancong <itancong@163.com>
2021-04-15 00:37:12 +0800

    04.15 macos

--- api-commons/src/main/java/cn/edu/pku/utils/JsonUtils.java
+++ api-commons/src/main/java/cn/edu/pku/utils/JsonUtils.java
@@ -1,10 +1,6 @@
 package cn.edu.pku.utils;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.annotation.JSONField;
-import lombok.*;
-
-import java.util.Date;
 
 public class JsonUtils {
 
