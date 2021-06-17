39e9084a595c12735816ff8ea8340d01364e6951
tancong <itancong@163.com>
2021-06-17 23:25:43 +0800

    06.17 windows

--- sql/hosts.sql
+++ sql/hosts.sql
@@ -2,11 +2,11 @@ USE attack_detection;
 
 DROP TABLE IF EXISTS `hosts`;
 
-CREATE TABLE detection_result(
+CREATE TABLE `hosts`(
     `id` BIGINT NOT NULL AUTO_INCREMENT,
     `ip` varchar(50) NOT NULL,
     `port` int NOT NULL,
     `status` int NOT NULL DEFAULT (0),
-    `state` int NOT NULL DEFAULT (1)
+    `state` int NOT NULL DEFAULT (1),
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
